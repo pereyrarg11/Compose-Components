c10d761080a3cd86edb47a7d55057fdef470b65a
gabriel-underdog <gabriel@underdog.mx>
2023-02-20 20:36:18 -0600

    chore: example of Text

--- app/src/main/java/com/pereyrarg11/composecomponents/MainActivity.kt
+++ app/src/main/java/com/pereyrarg11/composecomponents/MainActivity.kt
@@ -3,13 +3,20 @@ package com.pereyrarg11.composecomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.pereyrarg11.composecomponents.ui.theme.ComposeComponentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +29,61 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyText()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un ejemplo", style = TextStyle(fontFamily = FontFamily.Cursive))
+        // change text decoration through style attribute
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+                )
+            )
+        )
+        // change text decoration with the specific attribute
+        Text(
+            text = "Esto es un ejemplo",
+            textDecoration = TextDecoration.LineThrough
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            textDecoration = TextDecoration.Underline
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            textDecoration = TextDecoration.combine(
+                listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+            )
+        )
+        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeComponentsTheme {
-        Greeting("Android")
+        MyText()
     }
 }
